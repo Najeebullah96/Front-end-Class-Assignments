03ed9480dac7935f269875eff42dbe95620648bb
Najeebullah96 <108128096+Najeebullah96@users.noreply.github.com>
2022-10-22 12:42:42 +0430

    Password Generator App
    Module 3

---
+++ 9 Solo Project (Pass Gen)/script.js
@@ -0,0 +1,114 @@
+let genbtn = document.getElementById("generate_pass");
+let passOne = document.getElementById("pass_one");
+let passTwo = document.getElementById("pass_two");
+
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+genbtn.addEventListener("click", function () {
+  let password = "";
+  for (i = 0; i < 15; i++) {
+    var randomChar = Math.floor(Math.random() * characters.length);
+
+    password += characters[randomChar];
+  }
+  passOne.textContent = password;
+  password = "";
+  for (i = 0; i < 15; i++) {
+    var randomChar = Math.floor(Math.random() * characters.length);
+
+    password += characters[randomChar];
+  }
+  passTwo.textContent = password;
+});
